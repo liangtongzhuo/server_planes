2136235d1bc5b5aae8c10ae32d6c18a669f73b11
liangtongzhuo@gmail.com <liangtongzhuo@gmail.com>
2018-01-10 17:39:18 +0800

    飞机

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-let g_data = { arr: [] }; //全局数据保存
+let g_data = { arr: [], wreck: [] }; //全局数据保存
 const ws = require('ws');
 const wss = new ws.Server({ port: 4001 }, () => {
   console.log('服务器启动');
@@ -22,8 +22,12 @@ wss.on('connection', (ws, req) => {
   // console.log('url:', req.url);
 
   ws.on('message', (newData) => {
-    //更新状态
-    idUpdateAircraft(newData)
+    newData = JSON.parse(newData);
+    // 更新状态用户飞机位置
+    idUpdateAircraft(newData);
+    // 更敌机坠毁
+    idUpdateWreck(newData);
+
   });
 
   ws.on('error', (params) => {
@@ -57,8 +61,10 @@ function broadcast() {
   g_data.enemy = undefined;
   // 清空用户飞机
   g_data.arr = [];
-  
+  // 有用户断开
   g_data.close = false;
+  // 敌机坠毁
+  g_data.wreck = []
 };
 
 setInterval(broadcast, 1000 / 30);
@@ -76,7 +82,6 @@ setInterval(() => {
  * 判断 id 是否存在，设置飞机参数
  */
 function idUpdateAircraft(newData) {
-  newData = JSON.parse(newData);
   for (let i = 0; i < g_data.arr.length; i++) {
     if (g_data.arr[i].id === newData.id) {
       g_data.arr[i] = newData;
@@ -86,4 +91,13 @@ function idUpdateAircraft(newData) {
   g_data.arr.push(newData);
 }
 
+/**
+ * 飞机坠毁
+ */
+function idUpdateWreck(newData) {
+  for (let i = 0; i < newData.wreck.length; i++) {
+    if (g_data.wreck.indexOf(newData.wreck[i]) == -1)
+      g_data.wreck.push(newData.wreck[i]);
+  }
 
+}
