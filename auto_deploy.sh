5349db33ffa47933895c089bb630a4124a450871
ltz <liangtongzhuo@gmail.com>
2018-05-29 15:35:10 +0800

    自动部署 测试

---
+++ auto_deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# add the below line to crontab
+# */1 * * * * cd /home/ubuntu/node-live-api/current && ./update.sh > /dev/null
+
+git remote update
+
+LOCAL=$(git rev-parse @)
+
+REMOTE=$(git rev-parse "origin/master")
+
+if [ $LOCAL = $REMOTE ];then
+ echo "up-to-date" > /dev/null
+else
+ git checkout master
+ git pull && npm install && sudo pm2 reload server_planes
+fi
