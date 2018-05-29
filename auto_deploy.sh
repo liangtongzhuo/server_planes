8584928bc0a31d43d2bbcbe288004b72463cd115
ltz <liangtongzhuo@gmail.com>
2018-05-29 16:48:29 +0800

    自动部署 测试

--- auto_deploy.sh
+++ auto_deploy.sh
@@ -6,20 +6,18 @@
 # */1 * * * * cd ubuntu /home/ubuntu/code/server_planes && ./auto_deploy.sh > /dev/null
 # nohup ./auto_deploy.sh &
 
+
 while true
 do
-	git remote update
-
+  git remote update
 	LOCAL=$(git rev-parse @)
-
 	REMOTE=$(git rev-parse "origin/master")
-
 	if [ $LOCAL = $REMOTE ];then
 	 echo "up-to-date" > /dev/null
 	else
 	 git checkout master
 	 git pull && npm install && pm2 reload server_planes #当前 pm2 运行的名字
 	fi	
- sleep 30
+  sleep 30
 done
 
