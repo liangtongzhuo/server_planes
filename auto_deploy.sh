95dd06382bab95f86d5b1b529e7f3d649a2b02e1
ltz <liangtongzhuo@gmail.com>
2018-05-29 17:02:45 +0800

    自动部署 测试

--- auto_deploy.sh
+++ auto_deploy.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-
-# Ubuntu 定时执行 .sh 脚本
-# sudo vim /etc/crontab
-# 然后加入一下这行：
-# */1 * * * * cd ubuntu /home/ubuntu/code/server_planes && ./auto_deploy.sh > /dev/null
-# nohup ./auto_deploy.sh &
+# 后台启动 shell , 日志输出到 auto_deploy.log
 # nohup ./auto_deploy.sh > auto_deploy.log 2>&1 &
 
 while true
@@ -13,11 +8,11 @@ do
 	LOCAL=$(git rev-parse @)
 	REMOTE=$(git rev-parse "origin/master")
 	if [ $LOCAL = $REMOTE ];then
-	  echo "up-to-date" > /dev/null
+	  echo "up-to-date"
 	else
 	  git checkout master
 	  git pull && npm install && pm2 reload server_planes #当前 pm2 运行的名字
 	fi	
-  sleep 30
+  sleep 30 #每 30 秒循环一次
 done
 
